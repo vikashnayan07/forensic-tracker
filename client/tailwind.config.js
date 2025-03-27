a5a00313edb544c65542ca131bfbf745025a6bad
Vikash Nayan <imvikashnayan@gmail.com>
2025-03-27 18:48:21 +0530

    all the updates

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,7 +4,17 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}", // Scan all files in src/ for Tailwind classes
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        "fade-in": "fadeIn 1s ease-in-out forwards",
+      },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: "0", transform: "translateY(20px)" },
+          "100%": { opacity: "1", transform: "translateY(0)" },
+        },
+      },
+    },
   },
   plugins: [],
 };
